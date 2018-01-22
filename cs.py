348d43483b04632b93d5be3e5a9709d6dccf509a
realMiga <34860804+realMiga@users.noreply.github.com>
2018-01-22 06:36:10 +0000

    Rename cs.py to 先调用再输出

--- cs.py
+++
@@ -1,3 +0,0 @@
-def add(x,y,f):
-    return f(x) + f(y)
-print(add(-5,6,abs)) # 值与定义的变量参数一一对应 结果11
---
+++ 先调用再输出
@@ -0,0 +1,3 @@
+def add(x,y,f):
+    return f(x) + f(y)
+print(add(-5,6,abs)) # 值与定义的变量参数一一对应 结果11
