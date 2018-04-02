a63bb4c341d8b54fc3f140c2fc6151be45c37ca3
realMiga <34860804+realMiga@users.noreply.github.com>
2018-04-02 15:36:51 +0800

    给定日期的10天后时间戳

--- Lisence/test.py
+++ Lisence/test.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import time           # 计算某天是当年第几天
-try:
-
-   date = input('请输入日期：')
-   t = time.strptime(date, '%Y-%m-%d')
-   print(time.strftime('%j',t))
-except:
-    print('输入的格式错误，请重新输入!')
-    exit()
+import time,datetime    # 计算给定日期的10天后时间戳
+t = datetime.datetime(2014, 10, 25)
+timestamp = time.mktime(t.timetuple())
+timestamp += 10 * 3600 * 24
+t = time.localtime(timestamp)
+timeStr = time.strftime('%Y-%m-%d %H:%M:%S', t)
+print(timeStr)
