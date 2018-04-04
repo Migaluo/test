e0a2766e88ce40db50f359944de29a211b7708e1
realMiga <34860804+realMiga@users.noreply.github.com>
2018-04-04 14:56:05 +0800

    低级的输出成绩等级

--- Lisence/oop_1.py
+++ Lisence/oop_1.py
@@ -1,6 +1,20 @@
-class Solution(object):
-    def foo(self, s):   # 2.此时s接收到ss
-        def bar(a):     # 3.bar暂时没有调用不执行
-            print(s)    #  5、 输出s 在2步已赋值ss
-        bar("aa")       #  4、调用bar函数，传参是aa,但在此程序中没用到
-Solution().foo("ss")     # 1.调用Solution中foo方法 传的参数是ss
+class Student(object):
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+
+    def Get_score(self):
+        return self.score
+
+
+    def Get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
+bart = Student('bart',59)
+sun = Student('simon',90)
+print(bart.Get_score(),bart.Get_grade())
